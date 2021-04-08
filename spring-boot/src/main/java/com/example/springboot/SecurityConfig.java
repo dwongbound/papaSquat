cf26aee743feb99888b36e0e174eb47c33922dd8
Anjali Gopinathan <asgopina@usc.edu>
2021-04-07 20:14:21 -0700

    updated package names

--- spring-boot/src/main/java/com/example/springboot/SecurityConfig.java
+++ spring-boot/src/main/java/com/example/springboot/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.googledemo;
+package com.example.springboot;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
