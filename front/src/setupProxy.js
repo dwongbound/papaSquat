3cd81876e4c36be936e1400e7ddfab1707f54c0c
Natalie Pham <nataliepham525@gmail.com>
2021-04-16 00:24:20 -0700

    removed proxy

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/api/',
     createProxyMiddleware({
       target: 'http://localhost:8080',
       changeOrigin: true,
